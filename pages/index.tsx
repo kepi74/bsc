02a21554e39d181f9dd904498c62540232310710
Pavel Kepka <kepi@kepi.name>
2020-02-21 14:33:34 +0100

    delete example files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,8 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from 'react';
+import { NextPage } from 'next';
 
 const IndexPage: NextPage = () => {
-  return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  )
-}
+  return <h1>Index</h1>;
+};
 
-export default IndexPage
+export default IndexPage;
