0dd554ce93c8e40a7365687cf0dc3ff2cc7c3406
Pavel Kepka <kepi@kepi.name>
2020-03-04 08:27:30 +0100

    add useApiLazy

--- src/useApi.ts
+++ src/useApi.ts
@@ -21,6 +21,38 @@ export interface Error {
 
 export type ApiResponse<T> = NoData | Loading | Data<T> | Error;
 
+export function useApiLazy<T>(info: RequestInfo) {
+  const [requestState, setRequestState] = React.useState<ApiResponse<T>>({
+    type: 'NoData',
+  });
+  const [count, setCount] = React.useState(0);
+  const init = React.useRef<RequestInit | undefined>(undefined);
+
+  const callback = (reqInit?: RequestInit) => {
+    init.current = reqInit;
+    setCount((c) => c + 1);
+  };
+
+  const callApi = async () => {
+    setRequestState(() => ({ type: 'Loading' }));
+    try {
+      const response = await fetch(info, init.current);
+      const data = await response.json();
+      setRequestState(() => ({ type: 'Data', data }));
+    } catch (err) {
+      setRequestState(() => ({ type: 'Error', message: err.message }));
+    }
+  };
+
+  React.useEffect(() => {
+    if (count > 0) {
+      callApi();
+    }
+  }, [count]);
+
+  return [callback, requestState];
+}
+
 export default function useApi<T>(info: RequestInfo, init?: RequestInit) {
   const [requestState, setRequestState] = React.useState<ApiResponse<T>>({
     type: 'NoData',
